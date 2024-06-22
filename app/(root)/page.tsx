c64bcec1a41b11becd9416f3d071ea2e99b1673b
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-06-22 21:58:24 +0530

    product card page initialized

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,20 @@
+import Collections from "@/components/Collections";
 import Navbar from "@/components/Navbar";
+import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
-export default function Home(){
+export default function Home() {
   return (
     <>
-      <Image src={'/freya_banner.png'} alt="banner" width={2000} height={1000} className="w-screen h-[90%]" />
+      <Image
+        src={"/freya_banner.png"}
+        alt="banner"
+        width={2000}
+        height={1000}
+        className="w-screen h-[90%]"
+      />
+      <Collections/>
+      <ProductList/>
     </>
   );
 }
---
+++ components/ProductCard.tsx
@@ -0,0 +1,8 @@
+
+const ProductCard = ({product}:{product:ProductType}) => {
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
